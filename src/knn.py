a8ba1db15615f589269171791a8b2bbb08e955c3
Timofey-D <timofey.dankevich@gmail.com>
2022-05-19 19:47:20 +0300

    Recoded the class completely

--- src/knn.py
+++ src/knn.py
@@ -2,10 +2,10 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn import preprocessing
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
+import numpy as np
 
 
 class KNN:
-
 
     def __init__(self, k_nghbs, n_ths, train, valid, train_l, valid_l):
         
@@ -20,13 +20,13 @@ class KNN:
     def get_report(self):
         report = dict()
 
-        prediction = self.knn.predict(self.train)
+        prediction = self.knn.predict(self.valid)
         report.update( {'prediction' : prediction} )
 
         classification = classification_report(self.valid_l, prediction)
-        report.update( {'classification' : classification} )
-
-        conf_matrix = confusion_matrix(self.valid, prediction)
+        report.update( {'classification report' : classification} )
+        
+        conf_matrix = confusion_matrix(self.valid_l, prediction)
         report.update( {'confusion matrix' : conf_matrix} )
 
         accuracy = self.knn.score(self.valid, self.valid_l)
@@ -35,7 +35,11 @@ class KNN:
         return report
 
 
-    def train(self):
+    def model_info(self):
+        return self.knn.get_params()
+
+
+    def train_model(self):
 
         # To teach a model via Training group
         self.knn.fit(self.train, self.train_l)
