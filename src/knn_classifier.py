736d1935e761dc7276a33b253bb8e56cf9e683f0
Timofey-D <timofey.dankevich@gmail.com>
2022-05-19 19:47:45 +0300

    The entry point of the program

--- src/knn_classifier.py
+++ src/knn_classifier.py
@@ -1,27 +1,28 @@
 import os
 import sys
 
-from preprocessing import Preprocessing
-from knn import KNN
-import pandas as pd
+from mode import Mode
+from output import Output
 
 
-def write_to_csv(confusion_matrix, filename='result.csv'):
-
-    # To get a path to the result.csv
-    path_to_csv = os.path.join(os.getcwd(), filename)
-
-    # To create a dataframe with confusion matrix
-    df = pd.DataFrame(confusion_matrix)
-
-    # To create a csv file with the confusion matrix
-    df.to_csv(path_to_csv)
+def greeting():
+    print("The program includes 2 modes of execution:")
+    print("The mode 1 uses the validation set during training and random states are None.")
+    print("The mode 2 uses the test set during training and random state 1 is 6 and random state 2 is 33.")
 
 
 def main():
 
-    dataset = Preprocessing(sys.argv[1])
-    knn = KNN(5, 3)
+    greeting()
+    mode = int(input("Enter the program mode [1, 2]: "))
+
+    program = Mode(mode, sys.argv[1])
+    program.run_mode(5, 3)
+    knn = program.get_knn()
+    report = knn.get_report()
+
+    output = Output(report, program)
+    output.create_report_directory()
 
 
 if __name__ == '__main__':
